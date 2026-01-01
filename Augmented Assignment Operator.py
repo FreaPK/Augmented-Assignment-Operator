f95a264794bd052aa8960d83d091125c26effa1b
Rahul Bhardwaj <98464340+FreaPK@users.noreply.github.com>
2026-01-01 15:10:53 +0530

    Add files via upload

---
+++ Augmented Assignment Operator.py
@@ -0,0 +1,24 @@
+#Augmented Assignment Operator
+
+#basically i can increase/decrease or improve/impair a value of an assigned variable
+value=120
+value=value+2
+print(value)
+
+value=120
+value+=2
+print(value)
+
+value=120
+value-=2
+print(value)
+
+#we have to make sure that so and so variable has a former value to use augmented assignment operator
+
+value=120
+value*=2
+print(value)
+
+value=120
+value/=2
+print(value)
